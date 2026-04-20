cab074c0e7fa49a07fe6a3d95a4ae10722a16ea5
Olli <>
2026-04-20 18:56:56 +0200

    Github ci removed ... don't want the notifications if the runner doesn't run

--- .github/tests/setup/disable-nginx-service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-
-# Disable ES initialization in the test because we don't have an external ES service.
-echo "NGINX_ENABLED=false" > .env
-
-docker compose -f docker-compose.yml -f scenarios/disable-nginx-service.yml up --pull always --quiet-pull --detach
